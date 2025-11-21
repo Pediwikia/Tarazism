011e7c4bece4247e21aa4f2ddb9cebc5302a0828
Rocketbunny <rias.gremory7479@gmail.com>
2025-11-22 01:52:41 +0330

    Taraz

---
+++ Models/TarazQuiz.cs
@@ -0,0 +1,21 @@
+﻿namespace Tarazism.Models
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; } = "";
+        public List<string> Options { get; set; } = new();
+    }
+
+    public class QuizResult
+    {
+        public string Type { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string ImageUrl { get; set; } = "";
+    }
+
+    public class TarazQuiz
+    {
+        public List<QuizQuestion> Questions { get; set; } = new();
+        public List<QuizResult> Results { get; set; } = new();
+    }
+}
---
+++ Models/TarazRoadmap.cs
@@ -0,0 +1,15 @@
+﻿namespace Tarazism.Models
+{
+    public class RoadmapMilestone
+    {
+        public string Title { get; set; } = "";
+        public string Status { get; set; } = ""; // Planned / In Progress / Done
+        public string Icon { get; set; } = "";
+    }
+
+    public class TarazRoadmap
+    {
+        public string Year { get; set; } = "";
+        public List<RoadmapMilestone> Milestones { get; set; } = new();
+    }
+}
