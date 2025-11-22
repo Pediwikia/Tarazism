ca3d03f8314e679b5508612721b7a4ce464306ae
Rocketbunny <rias.gremory7479@gmail.com>
2025-11-22 10:41:30 +0330

    Profiles and DataBase

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Tarazism.Models
+{
+    public class TarazUser
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PasswordHash { get; set; } = ""; // for now simple hash
+
+        public string AvatarUrl { get; set; } = "/img/default-avatar.png";
+        public string Bio { get; set; } = "No bio yet.";
+
+        public DateTime Joined { get; set; } = DateTime.UtcNow;
+    }
+}
